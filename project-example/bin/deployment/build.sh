4ee80a6a6547f52f48cee5640d78ec5895ccabce
Alex-DA <Alex-DA@users.noreply.github.com>
2022-11-05 15:23:53 +0100

    add example project

---
+++ project-example/bin/deployment/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Build Project..."
+
+# Ex. docker-compose -f compose.yml -f compose.{{ENVIRONMENT}}.yml build
---
+++ project-example/bin/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Stop Development..."
+
+# Ex. docker-compose -f compose.yml -f compose.{{ENVIRONMENT}}.yml down
