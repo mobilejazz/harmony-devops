9812535ea59a2f56c195dba35dc53a519778d93c
Àlex A <Alex-DA@users.noreply.github.com>
2022-11-07 14:59:14 +0100

    DOC-3170 :: Feature :: DevOps Scripts standard Harmony Reference (#1)
    DOC-3170 :: Feature :: DevOps Scripts standard Harmony Reference (#1)

---
+++ project-example/bin/deployment/up.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Up Project..."
+
+# Ex. docker-compose -f compose.yml -f compose.{{ENVIRONMENT}}.yml up -d
---
+++ project-example/bin/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Start Development..."
+
+# Ex. docker-compose -f compose.yml -f compose.{{ENVIRONMENT}}.yml up
